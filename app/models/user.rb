8793347091ff33611e8f086d73bd52d8e16ba9ac
HijiriSato88 <satohiji0808@outlook.com>
2025-01-26 15:11:01 +0900

    validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+    validates :age, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
