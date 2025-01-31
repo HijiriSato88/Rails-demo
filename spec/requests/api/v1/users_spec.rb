5c82d6501e8fc2340b926ee4ddede6a5d4e6eac1
HijiriSato88 <satohiji0808@outlook.com>
2025-01-31 23:20:18 +0900

    fix indent

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -7,20 +7,18 @@ RSpec.describe 'Api::V1::UsersController', type: :request do
   describe 'GET /api/v1/index' do
     it 'returns all users' do
       get '/api/v1/index'
-
-    # 返り値(render json: users)を変数に格納
-    json = JSON.parse(response.body)
-    # 200okが返ってきたか確認
-    expect(response.status).to eq(200)
-    # 10件のデータが返ってきているかを確認
-    expect(json.length).to eq(5)
+      # 返り値(render json: users)を変数に格納
+      json = JSON.parse(response.body)
+      # 200okが返ってきたか確認
+      expect(response.status).to eq(200)
+      # 10件のデータが返ってきているかを確認
+      expect(json.length).to eq(5)
     end
   end
 
   describe 'GET /api/v1/:id/details' do
     it 'returns a user' do
       get "/api/v1/#{user_id}/details"
-
       json = JSON.parse(response.body)
       expect(response.status).to eq(200)
       expect(json['id']).to eq(user_id)
@@ -32,23 +30,23 @@ RSpec.describe 'Api::V1::UsersController', type: :request do
     let(:invalid_attributes) { { name: '', age: nil, email: 'invalid-email' } }
 
     context 'request is valid' do
-        it 'creates a new user' do
-            # valid_attributesをリクエストデータでPOSTリクエストを送り、Userモデルのレコード数が1増加することを検証
-            expect {
-            post '/api/v1/register', params: valid_attributes
-            }.to change(User, :count).by(1)
-            # HTTPレスポンスステータスコードが201 Createdであることを確認
-            expect(response.status).to eq(201)
-        end
+      it 'creates a new user' do
+        # valid_attributesをリクエストデータでPOSTリクエストを送り、Userモデルのレコード数が1増加することを検証
+        expect {
+        post '/api/v1/register', params: valid_attributes
+        }.to change(User, :count).by(1)
+        # HTTPレスポンスステータスコードが201 Createdであることを確認
+        expect(response.status).to eq(201)
+      end
     end
 
     context 'request is invalid' do
-        it 'creates a new user' do
-            expect {
-            post '/api/v1/register', params: invalid_attributes
-            }.to_not change(User, :count)
-            expect(response.status).to eq(422)
-        end
+      it 'creates a new user' do
+        expect {
+        post '/api/v1/register', params: invalid_attributes
+        }.to_not change(User, :count)
+        expect(response.status).to eq(422)
+      end
     end
   end
 
@@ -57,33 +55,33 @@ RSpec.describe 'Api::V1::UsersController', type: :request do
     let(:invalid_attributes) { {email: ""} }
     
     context 'is valid' do
-        it 'update the user' do
-            patch "/api/v1/#{user_id}/update", params: valid_attributes
-            json = JSON.parse(response.body)
-            expect(response.status).to eq(200)
-            expect(json['data']['name']).to eq("Update Taro")
-        end
+      it 'update the user' do
+        patch "/api/v1/#{user_id}/update", params: valid_attributes
+        json = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(json['data']['name']).to eq("Update Taro")
+      end
     end
 
     context 'when the request is invalid' do
-        it 'does not update the user' do
-            patch "/api/v1/#{user_id}/update", params: invalid_attributes
-            json = JSON.parse(response.body)
+      it 'does not update the user' do
+        patch "/api/v1/#{user_id}/update", params: invalid_attributes
+        json = JSON.parse(response.body)
 
-            expect(response.status).to eq(422) 
-            expect(json['data']).to have_key('email')
-        end
+        expect(response.status).to eq(422) 
+        expect(json['data']).to have_key('email')
+      end
     end
   end
 
   describe 'DELETE api/v1/:id/delete' do
     context 'success' do
-        it 'deletes the user' do
-            expect {
-              delete "/api/v1/#{user_id}/delete"
-            }.to change(User, :count).by(-1)
-            expect(response.status).to eq(200)
-        end
+      it 'deletes the user' do
+        expect {
+          delete "/api/v1/#{user_id}/delete"
+        }.to change(User, :count).by(-1)
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
